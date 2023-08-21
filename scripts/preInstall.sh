eed94d30678bf1e46fc6c4c00b12d343a6297758
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-21 15:15:34 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -9,21 +9,3 @@ chmod +x openslides
 ./openslides setup .
 
 # echo ${ADMIN_PASSWORD}> ./secrets/superadmin 
-
-
-cat <<EOT > ./servers.json
-{
-    "Servers": {
-        "1": {
-            "Name": "local",
-            "Group": "Servers",
-            "Host": "172.17.0.1",
-            "Port": 5432,
-            "MaintenanceDB": "postgres",
-            "SSLMode": "prefer",
-            "Username": "postgres",
-            "PassFile": "/pgpass"
-        }
-    }
-}
-EOT
