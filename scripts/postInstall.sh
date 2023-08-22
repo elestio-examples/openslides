676fda1ad9048024645deffab5c404c9a0f45ffb
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-22 12:03:11 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,12 +5,12 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready ..."
 sleep 30s;
 
-docker-compose exec -T database sh -c "psql -U postgres openslides <<EOF
-\c openslides
-UPDATE public.models
-SET data = jsonb_set(data, '{url}', '"${DOMAIN}"', false)
-WHERE data ->> 'id' = '1';
-EOF";
+# docker-compose exec -T database sh -c "psql -U postgres openslides <<EOF
+# \c openslides;
+# UPDATE public.models
+# SET data = jsonb_set(data, '{url}', '"${DOMAIN}"', false)
+# WHERE data ->> 'id' = '1';
+# EOF";
 
 # INSERT INTO public.events
 # (fqid, type, data, weight) VALUES('organization/1', 'update', '{"url": "${DOMAIN}"}', 1);
@@ -18,10 +18,12 @@ EOF";
 
 ./openslides initial-data
 ./openslides set-password -u 1 -p ${ADMIN_PASSWORD}
+./openslides set organization -f - <<< '[{ "id": 1, "url": "https://${DOMAIN}" }]'
+./openslides set organization -f - <<< '[{ "id": 1, "name": "Organization" }]'
+./openslides set user -f - <<< '[{ "id": 1, "email": "https://${ADMIN_EMAIL}" }]'
 
 # ./openslides set organization.update {url: 'https://yu.com', id: 1}
 # ./openslides set organization.update {name: 'Organization', id: 1}
-# openslides set meeting -f - <<< '[{ "id": 12, "name": "cool meeting" }]'
 # ./openslides set action -u 1 organization.update {url: "https://yu.com"}
 # ./openslides set organization.update {name: 'Organization'}
 
