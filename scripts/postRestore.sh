b948885f5fafa74503748b17ef2295cd2d070518
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-09-21 21:02:34 +0530

    Update postRestore.sh

--- scripts/postRestore.sh
+++ scripts/postRestore.sh
@@ -1,2 +1,10 @@
-#Restart the stack after files are restored
+#Restart the stack after the files are restored
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
 docker-compose up -d;
+
+#wait until the server is ready
+echo "Migration running ..."
+sleep 20s;
+./openslides migrations finalize
